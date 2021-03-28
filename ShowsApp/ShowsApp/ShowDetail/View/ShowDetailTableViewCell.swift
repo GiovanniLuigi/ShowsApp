64a84661a64ddc0ea287959106154afe0ebe3cd5
GiovanniLuigi <giovannibruno128@gmail.com>
2021-03-28 19:40:21 -0300

    Create show detail cell

---
+++ ShowsApp/ShowsApp/ShowDetail/View/ShowDetailTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ShowDetailTableViewCell.swift
+//  ShowsApp
+//
+//  Created by Giovanni Luidi Bruno on 28/03/21.
+//
+
+import UIKit
+
+class ShowDetailTableViewCell: UITableViewCell {
+    @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        showAnimatedSkeleton()
+    }
+    
+}
